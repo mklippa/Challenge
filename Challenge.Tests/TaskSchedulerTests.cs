08246443988e60f576cf47c0fa66fd8b09de1f15
Maxim Klippa <maxim.klippa@gmail.com>
2019-01-20 20:33:36 +0300

    complete set of tests

--- Challenge.Tests/TaskSchedulerTests.cs
+++ Challenge.Tests/TaskSchedulerTests.cs
@@ -106,8 +106,7 @@ namespace Challenge.Tests
         }
 
         [Test]
-        // complete all scheduled tasks in random order  when there are more parallel tasks
-        public async Task TestAsync()
+        public async Task Schedule_RunsAllTasksInRandomOrder_IfParallelismDegreeIsGreaterThenOrEqualToTasksCount()
         {
             // Arrange
             var expected = new[] { 1, 2, 3 };
@@ -117,7 +116,7 @@ namespace Challenge.Tests
             var task2 = new TestTask(started: actual, order: 2);
             var task3 = new TestTask(started: actual, order: 3);
 
-            _taskScheduler.Initialize(3);
+            _taskScheduler.Initialize(parallelTaskNumber: 3);
 
             // Act
             _taskScheduler.Schedule(task1, Priority.Low);
@@ -129,9 +128,8 @@ namespace Challenge.Tests
             CollectionAssert.AreEquivalent(expected, actual);
         }
 
-        // 1 x ||, 5 tasks, first - long, 2-5 correct order
         [Test]
-        public async Task Test2()
+        public async Task Schedule_RunsAllTasksInStrictOrder_IfParallelismDegreeIsEqualToOne()
         {
             // Arrange
             var expected = new[] { 1, 2, 3, 5, 4, 8, 7, 6 };
@@ -161,13 +159,23 @@ namespace Challenge.Tests
             // Assert
             CollectionAssert.AreEqual(expected, actual);
         }
+
         // 5 x ||, 5 tasks, first - long (not necessary), 1-5 random order, but devided in half
         [Test]
         public async Task Test3()
         {
             // Arrange
-            var expectedFirstPortion = new[] { 1, 2, 3, 4 };
-            var expectedSecondPortion = new[] { 5, 6, 7, 8 };
+            // Input:    Output:
+            // 1. High   1. High
+            // 2. High   2. High
+            // 3. High   3. High
+            // 4. High   5. Normal
+            // 5. Normal 4. High
+            // 6. Low    8. High
+            // 7. Normal 7. Normal
+            // 8. High   6. Low
+            var expectedFirstPortion = new[] { 1, 2, 3, 5 };
+            var expectedSecondPortion = new[] { 4, 8, 7, 6 };
 
             var actual = new List<int>();
             // the first portion
@@ -187,10 +195,10 @@ namespace Challenge.Tests
             // schedule the first portion
             _taskScheduler.Schedule(task1, Priority.High);
             _taskScheduler.Schedule(task2, Priority.High);
-            _taskScheduler.Schedule(task3, Priority.Normal);
+            _taskScheduler.Schedule(task3, Priority.High);
             _taskScheduler.Schedule(task4, Priority.High);
             // schdule the second portion
-            _taskScheduler.Schedule(task5, Priority.High);
+            _taskScheduler.Schedule(task5, Priority.Normal);
             _taskScheduler.Schedule(task6, Priority.Low);
             _taskScheduler.Schedule(task7, Priority.Normal);
             _taskScheduler.Schedule(task8, Priority.High);
@@ -199,6 +207,66 @@ namespace Challenge.Tests
             // Assert
             CollectionAssert.AreEquivalent(expectedFirstPortion, actual.Take(4));
             CollectionAssert.AreEquivalent(expectedSecondPortion, actual.Skip(4));
+        } 
+
+        [Test]
+        public async Task TestBuz3()
+        {
+            // Arrange
+            // Input:    Output:
+            // 1. Low    2. High
+            // 2. High   5. High
+            // 3. Normal 7. High
+            // 4. Low    3. Normal
+            // 5. High   8. High
+            // 6. Normal 9. High
+            // 7. High   6. Normal
+            // 8. High   1. Low
+            // 9. High   4. Low
+            var expectedFirstPortion = new[] { 2, 5, 7 };
+            var expectedSecondPortion = new[] { 3, 8, 9 };
+            var expectedThirdPortion = new[] { 6, 1, 4};
+
+            var actual = new List<int>();
+            // the first portion
+            var task1 = new TestTask(started: actual, order: 1);
+            var task2 = new TestTask(started: actual, order: 2);
+            var task3 = new TestTask(started: actual, order: 3);
+            // the second portion
+            var task4 = new TestTask(started: actual, order: 4);
+            var task5 = new TestTask(started: actual, order: 5);
+            var task6 = new TestTask(started: actual, order: 6);
+
+            // the third portion
+            var task7 = new TestTask(started: actual, order: 7);
+            var task8 = new TestTask(started: actual, order: 8);
+            var task9 = new TestTask(started: actual, order: 9);
+
+            // parallelTaskNumber == portion size
+            _taskScheduler.Initialize(parallelTaskNumber: 3);
+
+            // Act
+            // schedule the first portion
+            _taskScheduler.Schedule(task1, Priority.Low);
+            _taskScheduler.Schedule(task2, Priority.High);
+            _taskScheduler.Schedule(task3, Priority.Normal);
+
+            // schdule the second portion
+            _taskScheduler.Schedule(task4, Priority.Low);
+            _taskScheduler.Schedule(task5, Priority.High);
+            _taskScheduler.Schedule(task6, Priority.Normal);
+            
+            // schedule the third portion
+            _taskScheduler.Schedule(task7, Priority.High);
+            _taskScheduler.Schedule(task8, Priority.High);
+            _taskScheduler.Schedule(task9, Priority.High);
+            await _taskScheduler.Stop(CancellationToken.None);
+
+            // Assert
+            System.Diagnostics.Debug.Print($"{string.Join(", ", actual)}\n");
+            CollectionAssert.AreEquivalent(expectedFirstPortion, actual.Take(3));
+            CollectionAssert.AreEquivalent(expectedSecondPortion, actual.Skip(3).Take(3));
+            CollectionAssert.AreEquivalent(expectedThirdPortion, actual.Skip(6));
         } 
 
         // cancell - отменили сразу после запуска
